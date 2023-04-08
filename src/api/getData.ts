0d8c2c04da2887fbca5548a47f1e22980e0a63ea
AntoniBasa <antoni.geratti@gmail.com>
2023-04-08 13:03:33 +0300

    made function getAnalytics immutable

--- src/api/getData.ts
+++ src/api/getData.ts
@@ -38,91 +38,93 @@ export const getAllTasks: () => ITask[] = () => {
       status: "in progress",
     },
     {
-        title: "Task 6",
-        description: "Lorem ipsum dolor sit amet",
-        assignee: "Alice",
-        dueDate: new Date(2023, 3, 10),
-        status: "open",
-      },
-      {
-        title: "Task 7",
-        description: "Consectetur adipiscing elit",
-        assignee: "Bob",
-        dueDate: new Date(2023, 3, 15),
-        status: "in progress",
-      },
-      {
-        title: "Task 8",
-        description: "Sed do eiusmod tempor incididunt",
-        assignee: "Charlie",
-        dueDate: new Date(2023, 3, 18),
-        status: "completed",
-      },
-      {
-        title: "Task 9",
-        description: "Ut enim ad minim veniam",
-        assignee: "Alice",
-        dueDate: new Date(2023, 3, 20),
-        status: "open",
-      },
-      {
-        title: "Task 10",
-        description: "Duis aute irure dolor in reprehenderit",
-        assignee: "Dave",
-        dueDate: new Date(2023, 3, 27),
-        status: "in progress",
-      }
+      title: "Task 6",
+      description: "Lorem ipsum dolor sit amet",
+      assignee: "Alice",
+      dueDate: new Date(2023, 3, 10),
+      status: "open",
+    },
+    {
+      title: "Task 7",
+      description: "Consectetur adipiscing elit",
+      assignee: "Bob",
+      dueDate: new Date(2023, 3, 15),
+      status: "in progress",
+    },
+    {
+      title: "Task 8",
+      description: "Sed do eiusmod tempor incididunt",
+      assignee: "Charlie",
+      dueDate: new Date(2023, 3, 18),
+      status: "completed",
+    },
+    {
+      title: "Task 9",
+      description: "Ut enim ad minim veniam",
+      assignee: "Alice",
+      dueDate: new Date(2023, 3, 20),
+      status: "open",
+    },
+    {
+      title: "Task 10",
+      description: "Duis aute irure dolor in reprehenderit",
+      assignee: "Dave",
+      dueDate: new Date(2023, 3, 27),
+      status: "in progress",
+    },
   ];
 };
 
 export const getAnalytics = () => {
-  return getAllTasks().reduce(
-    (acc: IAnalytics, task) => {
-      if (task.status.includes(" ")) {
-        let modifiedStatusName = task.status
-          .split(" ")
-          .map((word, index) =>
-            index === 0 ? word : word.charAt(0).toUpperCase() + word.slice(1)
-          )
-          .join("");
-        if (acc.taskStatus[modifiedStatusName]) {
-          acc.taskStatus[modifiedStatusName]++;
-        } else {
-          acc.taskStatus[modifiedStatusName] = 1;
-        }
-      } else if (acc.taskStatus[task.status]) {
-        acc.taskStatus[task.status]++;
+  const tasks = getAllTasks();
+
+  const initialAnalytics = {
+    taskStatus: {},
+    assigneeStats: {},
+    dueDateStats: {},
+  };
+
+  return tasks.reduce((acc: IAnalytics, task) => {
+    const newAnalytics = Object.assign({}, acc);
+    if (task.status.includes(" ")) {
+      let modifiedStatusName = task.status
+        .split(" ")
+        .map((word, index) =>
+          index === 0 ? word : word.charAt(0).toUpperCase() + word.slice(1)
+        )
+        .join("");
+      if (newAnalytics.taskStatus[modifiedStatusName]) {
+        newAnalytics.taskStatus[modifiedStatusName]++;
       } else {
-        acc.taskStatus[task.status] = 1;
+        newAnalytics.taskStatus[modifiedStatusName] = 1;
       }
-
-      if (acc.assigneeStats[task.assignee]) {
-        acc.assigneeStats[task.assignee]++;
-      } else {
-        acc.assigneeStats[task.assignee] = 1;
-      }
-
-      const janFirst = new Date(task.dueDate.getFullYear(), 0, 1);
-
-      const diffDays = Math.round(
-        (task.dueDate.getTime() - janFirst.getTime()) / (1000 * 60 * 60 * 24)
-      );
-      const week = Math.ceil((janFirst.getDay() + 1 + diffDays) / 7);
-
-      if (acc.dueDateStats[`week${week}`]) {
-        acc.dueDateStats[`week${week}`]++;
-      } else {
-        acc.dueDateStats[`week${week}`] = 1;
-      }
-
-      return acc;
-    },
-    {
-      taskStatus: {},
-      assigneeStats: {},
-      dueDateStats: {},
+    } else if (newAnalytics.taskStatus[task.status]) {
+      newAnalytics.taskStatus[task.status]++;
+    } else {
+      newAnalytics.taskStatus[task.status] = 1;
     }
-  );
+
+    if (newAnalytics.assigneeStats[task.assignee]) {
+      newAnalytics.assigneeStats[task.assignee]++;
+    } else {
+      newAnalytics.assigneeStats[task.assignee] = 1;
+    }
+
+    const janFirst = new Date(task.dueDate.getFullYear(), 0, 1);
+
+    const diffDays = Math.round(
+      (task.dueDate.getTime() - janFirst.getTime()) / (1000 * 60 * 60 * 24)
+    );
+    const week = Math.ceil((janFirst.getDay() + 1 + diffDays) / 7);
+
+    if (newAnalytics.dueDateStats[`week${week}`]) {
+      newAnalytics.dueDateStats[`week${week}`]++;
+    } else {
+      newAnalytics.dueDateStats[`week${week}`] = 1;
+    }
+
+    return newAnalytics;
+  }, initialAnalytics);
 };
 
 export const getAssigneeToTasks = (assignee: string) => {
